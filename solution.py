51c61498c97c7c9ed00c2d203b3d3968f772134a
Muskaan Parikh <muskaan.parikh@bignited.be>
2025-05-30 15:44:59 +0200

    Feat: solution

---
+++ solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in nums and i is not nums.index(complement):
+            return [nums.index(complement), i]
+
+print(twoSum([2,7,11,15], 9))
+print(twoSum([3,2,4], 6))
+print(twoSum([3,3], 6))
